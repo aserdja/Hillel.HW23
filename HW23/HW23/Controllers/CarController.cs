8813c0fc2563f2e6775c1a897936839462b1c557
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-11-03 15:27:20 +0200

    Added StatusCode responces

--- HW23/HW23/Controllers/CarController.cs
+++ HW23/HW23/Controllers/CarController.cs
@@ -3,6 +3,7 @@ using HW23.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.IdentityModel.Tokens;
 
 namespace HW23.Controllers
@@ -14,25 +15,49 @@ namespace HW23.Controllers
 		private readonly AppDbContext _context = context;
 
 		[HttpGet]
-		public async Task<IEnumerable<Car>> GetAllAsync()
+		public async Task<IActionResult> GetAllAsync()
 		{
-			return await _context.Cars.ToListAsync();
+			try
+			{
+				return Ok(await _context.Cars.ToListAsync());
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, $"{ex.Message}");
+			}
+			
 		}
 
 		[HttpGet("{id}")]
-		public async Task<Car?> GetByIdAsync(int id)
+		public async Task<IActionResult> GetByIdAsync(int id)
 		{
-			return await _context.Cars.FirstOrDefaultAsync(c => c.Id.Equals(id));
+			var result = await _context.Cars.FirstOrDefaultAsync(c => c.Id.Equals(id));
+			if (result != null)
+			{
+				return Ok(result);
+			}
+			return NotFound();
+			
 		}
 
 		[HttpPost]
-		public async Task Add([FromForm] Car car)
+		public async Task<IActionResult> Add([FromForm] Car car)
 		{
-			if (ModelState.IsValid)
+			try
 			{
-				_context.Cars.Add(car);
-				await _context.SaveChangesAsync();
+				if (ModelState.IsValid)
+				{
+					_context.Cars.Add(car);
+					await _context.SaveChangesAsync();
+				}
+
+				return Ok();
 			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, $"{ex.Message}");
+			}
+			
 		}
 
 		[HttpPut]
